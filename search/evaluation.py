30c466a9a959ad454957c2bb273329ad491bbf74
fanshukai <fanshukai@fanshukais-MacBook-Pro.local>
2020-12-08 08:38:13 +0800

    Dec 8. evaluation modification

--- search/evaluation.py
+++ search/evaluation.py
@@ -3,6 +3,7 @@ import random
 import nltk
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
+from rake_nltk import Rake
 
 nltk.download('punkt')
 
@@ -28,10 +29,17 @@ def gid_to_fake_query(gid):
         fake_query = game_df[game_df['appid'] == gid]['short_description'].values[0]
     except:
         fake_query = game_df[game_df['appid'] == gid]['short_description'].values
-    print(fake_query)
-    tokens = word_tokenize(fake_query)
-    token_samples = random.sample(tokens, int(len(tokens) * 0.1))
-    fake_query = ' '.join(token for token in token_samples)
+    # print(fake_query)
+    # tokens = word_tokenize(fake_query)
+    # token_samples = random.sample(tokens, int(len(tokens) * 0.1))
+    r = Rake()
+    r.extract_keywords_from_text(fake_query)
+    token_samples = r.get_ranked_phrases()
+    if len(token_samples) < 2:
+        fake_query = ' '.join(token for token in token_samples)
+    else:
+        token_samples = random.sample(token_samples, 2)
+        fake_query = ' '.join(token for token in token_samples)
     return fake_query
 
 
@@ -40,6 +48,7 @@ for uid in users:
     
     last_gid = find_last_game(uid)
     fake_query =gid_to_fake_query(last_gid)
+    print(fake_query)
     gname = gid_to_name(last_gid)
     retrieved_games = userBasedRetrieval(uid, fake_query)
 
@@ -53,7 +62,7 @@ for uid in users:
         new_rank = -1
     print(original_rank)
     print(new_rank)
-    # if original_rank > new_rank:
-    #     pdb.set_trace()
-
+    if original_rank > new_rank:
+        pdb.set_trace()
+        # retrieved_games[['name', 'score', 'new_score', 'original_rank', 'new_rank']].sort_values('new_rank')
 
